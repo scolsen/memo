f99e495711be5d55dc4cfb29b4accda88b66a1a5
Scott Olsen <scg.olsen@gmail.com>
2024-02-23 13:49:54 -0500

    Feat: Support invocation in pipes
    On nix like systems, the file mode for stdin differs when the user
    invokes a program directly in comparison to when they invoked the same
    program as part of a pipe. This commit adds routines to detect which
    scenario we're operating under, and adjusts behaviors and argument
    expectations accordingly.
    
    Most importantly, we take the value to be 'memorized' off of piped stdin
    input when the program is invoked as part of a pipeline, and we exit if
    the user accidentally adds an additional argument. When we are not part
    of a pipeline, we expect an argument, and otherwise print the memorized
    value.

--- memo.go
+++ memo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+        "io"
 	"fmt"
 	"os"
 	"strings"
@@ -9,6 +10,29 @@ import (
 const memofile = ".memo"
 
 var memoloc = "/tmp/" + memofile
+
+func main() {
+   args := os.Args[1:] // ignore the program name
+   // If stdin is a TTY and the user provided no argument, there is possibly
+   // data fed through stdin by other means, e.g. via a pipe: command | memo ...
+   // Attempt to fetch an input argument from stdin directly.
+   if !isTTY(os.Stdin) {
+     // No argument should be passed if we're not operating in TTY mode.
+     if len(args) > 0 {
+       os.Exit(1)
+     }
+     writeInputFromStdin()
+     os.Exit(0)
+   }
+
+   // Operation in direct invocation TTY mode.
+   switch len(args) {
+     case 0:
+       read()
+     default:
+       write(args)
+   } 
+}
 
 func write(args []string) {
 	content := strings.Join(args, " ")
@@ -33,12 +57,22 @@ func read() {
 	fmt.Printf("%s", buf)
 }
 
-func main() {
-	switch len(os.Args) {
-	case 1:
-		read()
-	default:
-		args := os.Args[1:]
-		write(args)
-	}
+// writeInputFromStdin attempts to write data passed on stdin to the memo file.
+func writeInputFromStdin() {
+  buf, err := io.ReadAll(os.Stdin)
+  if err != nil {
+    os.Exit(1)
+  }
+  write([]string{string(buf)})
 }
+
+// isTTY returns true if the given file is a terminal emulator.
+func isTTY(file *os.File) bool {
+  fi, err := file.Stat()
+  if err != nil {
+    return false
+  }
+
+  return fi.Mode() & os.ModeCharDevice != 0
+}
+
